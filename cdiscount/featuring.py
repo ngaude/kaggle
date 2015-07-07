b77f084436a4641b6c1391340b754a25f29e92ac
ngaude <oktopela@yahoo.fr>
2015-07-07 03:24:30 +0200

    desappointing results 70/107... 15,9%

--- cdiscount/featuring.py
+++ cdiscount/featuring.py
@@ -27,6 +27,9 @@ fpath = 'E:/workspace/data/cdiscount/'
 ifname = fpath+'training.csv'
 ofname = fpath+'training.tsv'
 tfname = fpath+'training4M.tsv'
+sfname = fpath+'trainingShuffle.tsv'
+rfname = fpath+'test.csv'
+resultat = fpath+'resultat.csv'
 
 if not os.path.isfile(ofname):
     print 'creating :',ofname
@@ -36,23 +39,20 @@ if not os.path.isfile(ofname):
                 print i
             f.write(l.translate(maketrans(';','\t')))        
 
-if not os.path.isfile(tfname):
+if not os.path.isfile(tfname) or not os.path.isfile(sfname):
     print 'creating :',tfname
     df = pd.read_csv(ofname,sep='\t')
-    df.index = np.random.permutation(df.index)
+    df = df.reindex(np.random.permutation(df.index))
+    df.to_csv(sfname,sep='\t')
     df.head(4000000).to_csv(tfname,sep='\t')
 
-df = pd.read_csv(tfname,sep='\t')
+nb_txt = 50000
+nb_train = 100000
+nb_test = 20000
+
+df = pd.read_csv(sfname,sep='\t',nrows = nb_txt+nb_train+nb_test)
     
-vectorizer = TfidfVectorizer(
-    min_df = 0.00009,
-    max_features=200000,
-    stop_words=None, # 'french' is not supported so far
-    smooth_idf=True,
-    norm='l2',
-    sublinear_tf=False,
-    use_idf=True,
-    ngram_range=(1,3))
+
 
 intab = string.punctuation
 outtab = ' '*len(intab)
@@ -73,6 +73,8 @@ class iterText(object):
     
     def __iter__(self):
         for row_index, row in self.df.iterrows():
+            if row_index%1000==0:
+                print row_index
             d = m = l = ''
             if type(row.Description) is str:
                 d = text_normed(row.Description)
@@ -85,21 +87,56 @@ class iterText(object):
     
     def __len__(self):
         return len(self.df)
-        
-nb_train = 1000000
-nb_test = 10000
+  
+def cat_freq(df):
+    # compute Categorie3 classe frequency
+    sfreq = len(df)*1.
+    g = df.groupby('Categorie3').Libelle.count()/sfreq
+    return dict(g)
 
 train = df.head(nb_train)
 test = df.tail(nb_test)
+txt = df[nb_train:nb_train+nb_txt]
 
-X = vectorizer.fit_transform(iterText(train))
+vectorizer = TfidfVectorizer(
+    min_df = 0.00009,
+    max_features=nb_txt,
+    stop_words=None, # 'french' is not supported so far
+    smooth_idf=True,
+    norm='l2',
+    sublinear_tf=False,
+    use_idf=True,
+    ngram_range=(1,3))
+
+vectorizer.fit(iterText(txt))
+
+X = vectorizer.transform(iterText(train))
 y = train.Categorie3
 clf = SGDClassifier()
-clf.fit(X,y)
+clf.fit(X,y,class_weight = cat_freq(train))
 print clf.score(X, y)
 
 Xt = vectorizer.transform(iterText(test))
 yt = test.Categorie3
 print clf.score(Xt, yt)
 
-    
+#######################
+# RESULTAT SUBMISSION #
+#######################
+
+#rdf = pd.read_csv(rfname,sep=';')
+#Xtest = vectorizer.transform(iterText(rdf))
+#rdf['Id_Produit']=rdf['Identifiant_Produit']
+#rdf['Id_Categorie'] = clf.predict(Xtest)
+#rdf = rdf[['Id_Produit','Id_Categorie']]
+#rdf.to_csv(resultat,sep=';',index=False)
+
+##############
+# VALIDATION #
+##############
+vdf = pd.read_csv(sfname,skiprows = (nb_text+nb_train+nb_txt)+1000000,nrows=50000,sep=';')
+Xv = vectorizer.transform(iterText(vdf))
+yv = vdf.Categorie3
+print clf.score(Xv, yv)
+
+
