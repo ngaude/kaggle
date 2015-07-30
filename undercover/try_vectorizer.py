75863c579f4eb2113a8a35a63e55c0b12639288f
ngaude <oktopela@yahoo.fr>
2015-07-30 18:55:25 +0200

    first adasyn try with 200samples/class.... pending for about 4h ....

--- undercover/try_vectorizer.py
+++ undercover/try_vectorizer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from utils import ddir,header,add_txt,MarisaTfidfVectorizer,adasyn_sample
+from utils import ddir,header,add_txt,MarisaTfidfVectorizer,adasyn_sample,cat3tocat1
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -10,6 +10,7 @@ from sklearn.externals import joblib
 from os.path import basename
 import time
 import random
+from joblib import Parallel, delayed
 
 def vectorizer(txt):
     vec = MarisaTfidfVectorizer(
@@ -75,15 +76,14 @@ for i,cat in enumerate(np.unique(Y)):
 
 Xt = sparse.vstack(Xt) 
 assert Xt.shape[0] == len(Yt)
-
 rows = random.sample(Xt,Xt.shape[0])
-
-# TODO : beware...
-Xt[rows]
-
 Xt = Xt[rows]
-
 joblib.dump((vec,Xs,Ys),ddir+'joblib/vecXtYt')
+
+#################################################
+# TRAINING START HERE
+#################################################
+
 
 def training_stage1(X,Y,Xv,Yv):
     fname = ddir + 'joblib/stage1'
@@ -118,3 +118,184 @@ def training_stage3(X,Y,Xv,Yv,cat,i):
     joblib.dump((labels,cla),fname)
     del cla
     return (sct,scv)
+
+(vec,ID,X,Y) = joblib.load(ddir+'joblib/vecIDXY')
+dfvalid = pd.read_csv(ddir+'validation_normed.csv',sep=';',names = header()).fillna('')
+add_txt(dfvalid)
+Xv = vec.transform(dfvalid.txt)
+Yv = dfvalid.Categorie3
+dt = -time.time()
+sct,scv = training_stage1(X,Y,Xv,Yv)
+dt += time.time()
+
+print '**********************************'
+print 'stage1 elapsed time :',dt
+print 'stage1 training score :',sct
+print 'stage1 validation score :',scv
+print '**********************************'
+
+# training parallel stage3
+
+Z = map(lambda c:cat3tocat1[c], Y)
+Zv = map(lambda c:cat3tocat1[c], Yv)
+cat1 = np.unique(Z)
+XYs = []
+XYvs = []
+
+for cat in cat1:
+    indices = np.nonzero(Z==cat)
+    XYs.append((X[indices],Y[indices]))
+    indices = np.nonzero(Zv==cat)
+    XYvs.append((Xv[indices],Yv[indices]))
+
+dt = -time.time()
+scs = Parallel(n_jobs=3)(delayed(training_stage3)(XY[0],XY[1],XYv[0],XYv[1],cat,i) for i,(XY,XYv,cat) in enumerate(zip(XYs,XYvs,cat1)))
+dt += time.time()
+
+sct = np.median([s for s in zip(*scs)[0] if s>=0])
+scv = np.median([s for s in zip(*scs)[1] if s>=0])
+
+print '**********************************'
+print 'stage3 elapsed time :',dt
+print 'stage3 training score :',sct
+print 'stage3 validation score :',scv
+print '**********************************'
+
+#######################
+# predicting
+#######################
+
+# predict : 
+# P(Categorie3) = P(Categorie1) *  P(Categorie3|Categorie1)
+
+def log_proba(df,vec,cla):
+    assert 'txt' in df.columns
+    X = vec.transform(df.txt)
+    lp = cla.predict_log_proba(X)
+    return (cla.classes_,lp)
+
+dfvalid = pd.read_csv(ddir+'validation_normed.csv',sep=';',names = header()).fillna('')
+add_txt(dfvalid)
+Xv = vec.transform(dfvalid.txt)
+Yv = dfvalid.Categorie3
+
+dftest = pd.read_csv(ddir+'test_normed.csv',sep=';',names = header()).fillna('')
+add_txt(dftest)
+Xt = vec.transform(dftest.txt)
+Yt = dftest.Categorie3
+
+#######################
+# stage 1 log proba filling
+#######################
+stage1_log_proba_valid = np.full(shape=(Xv.shape[0],cat1count),fill_value = -666.,dtype = float)
+stage1_log_proba_test  = np.full(shape=(Xf.shape[0],cat1count),fill_value = -666.,dtype = float)
+
+fname = ddir + 'joblib/stage1'
+(labels,cla) = joblib.load(fname)
+(classes,lpv) = log_proba(dfvalid,vec,cla)
+(classes,lpt) = log_proba(dftest,vec,cla)
+for i,k in enumerate(classes):
+    j = cat1toi[k]
+    stage1_log_proba_valid[:,j] = lpv[:,i]
+    stage1_log_proba_test[:,j] = lpt[:,i]
+
+del labels,cla
+
+
+#######################
+# stage 3 log proba filling
+#######################
+stage3_log_proba_valid = np.full(shape=(Xv.shape[0],cat3count),fill_value = -666.,dtype = float)
+stage3_log_proba_test  = np.full(shape=(Xf.shape[0],cat3count),fill_value = -666.,dtype = float)
+
+for ii,cat in enumerate(itocat1):
+    fname = ddir + 'joblib/stage3_'+str(cat)
+    print '-'*50
+    print 'predicting',basename(fname),':',ii,'/',len(itocat1)
+    if not isfile(fname): 
+        continue
+    (labels,cla) = joblib.load(fname)
+    if len(labels)==1:
+        k = labels[0]
+        j = cat3toi[k]
+        stage3_log_proba_valid[:,j] = 0
+        stage3_log_proba_test[:,j] = 0
+        continue
+    (classes,lpv) = log_proba(dfvalid,vec,cla)
+    (classes,lpt) = log_proba(dftest,vec,cla)
+    for i,k in enumerate(classes):
+        j = cat3toi[k]
+        stage3_log_proba_valid[:,j] = lpv[:,i]
+        stage3_log_proba_test[:,j] = lpt[:,i]
+    del labels,cla
+
+print '>>> dump stage1 & stage2 log_proba'
+joblib.dump((stage1_log_proba_valid,stage3_log_proba_valid),ddir+'/joblib/log_proba_valid')
+joblib.dump((stage1_log_proba_test,stage3_log_proba_test),ddir+'/joblib/log_proba_test')
+print '<<< dump stage1 & stage2 log_proba'
+
+
+##################
+# (stage1_log_proba_valid,stage3_log_proba_valid) = joblib.load(ddir+'/joblib/log_proba_valid')
+# (stage1_log_proba_test,stage3_log_proba_test) = joblib.load(ddir+'/joblib/log_proba_test')
+##################
+
+##################
+# bayes rulez ....
+##################
+
+assert stage3_log_proba_valid.shape[1] == stage3_log_proba_test.shape[1]
+
+for i in range(stage3_log_proba_valid.shape[1]):
+    cat3 = itocat3[i]
+    cat1 = cat3tocat1[cat3]
+    j = cat1toi[cat1]
+    stage3_log_proba_valid[:,i] += stage1_log_proba_valid[:,j]
+    stage3_log_proba_test[:,i] += stage1_log_proba_test[:,j]
+
+predict_cat1_valid = [itocat1[i] for i in np.argmax(stage1_log_proba_valid,axis=1)]
+predict_cat3_valid = [itocat3[i] for i in np.argmax(stage3_log_proba_valid,axis=1)]
+predict_cat1_test = [itocat1[i] for i in np.argmax(stage1_log_proba_test,axis=1)]
+predict_cat3_test = [itocat3[i] for i in np.argmax(stage3_log_proba_test,axis=1)]
+
+def submit(df,Y):
+    submit_file = ddir+'resultat.csv'
+    df['Id_Produit']=df['Identifiant_Produit']
+    df['Id_Categorie'] = Y
+    df= df[['Id_Produit','Id_Categorie']]
+    df.to_csv(submit_file,sep=';',index=False)
+
+score_cat1 = sum(dfvalid.Categorie1 == predict_cat1_valid)*1.0/len(dfvalid)
+score_cat3 = sum(dfvalid.Categorie3 == predict_cat3_valid)*1.0/len(dfvalid)
+print 'validation score :',score_cat1,score_cat3
+submit(dftest,predict_cat3_test)
+
+##########################
+# reference model score  #
+##########################
+# NOTE
+# NOTE
+# NOTE
+# NOTE
+#################################################
+# stage1 elapsed time : 966.20471406
+# stage1 training score : 0.9633
+# stage1 validation score : 0.874375015096
+# stage3 elapsed time : ~1500
+# stage3 training score : 0.984027777778
+# stage3 validation score : 0.863612147043
+# validation score : 0.874375015096 0.68585299872
+# (result30.csv) test score : 63,99060%
+#################################################
+#
+#################################################
+# stage1 elapsed time : 448.81675601
+# stage1 training score : 0.9569
+# stage1 validation score : 0.874882249221
+# stage3 elapsed time : 765.464223146
+# stage3 training score : 0.9839
+# stage3 validation score : 0.857221006565
+# validation score : 0.874882249221 0.684452066375
+# (result31.csv) test score : 64,01352%
+#################################################
+
