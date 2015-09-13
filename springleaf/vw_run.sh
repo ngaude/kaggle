3ae33cf171fbb4d4d4ba3ae18b08bda2afc017bd
ngaude <oktopela@yahoo.fr>
2015-09-13 19:09:11 +0200

    tidy up

--- springleaf/vw_run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-rm data/train.cache data/data.model
-
-vw -b 27 --passes 50 --cache_file data/train.cache -f data/data.model -d data/vw_train.dat --loss_function logistic
-
-vw -t  -i data/data.model -d data/vw_test.dat -p data/vw_test.pred --loss_function logistic
-
